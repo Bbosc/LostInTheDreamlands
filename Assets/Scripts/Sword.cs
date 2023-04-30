6651e4d52e63dec71a80f94e343bf5159d76f16d
baudou <baudouin.bosc@gmail.com>
2023-05-01 01:25:57 +0200

    small cleaning

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,27 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    protected int tip_index = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    protected float force_multiplicator = 20.0f;
     private void OnCollisionEnter(Collision col)
     {
-        Debug.Log(col.GetContact(0).normal);
-        //this.GetComponentsInChildren<Collider>()[tip_index].enabled = true;
-        col.rigidbody.AddForce( - col.GetContact(0).normal * 20, ForceMode.Impulse);
+        col.rigidbody.AddForce( - col.GetContact(0).normal * force_multiplicator, ForceMode.Impulse);
         col.rigidbody.useGravity = true;
 
     }
@@ -29,10 +13,6 @@ public class Sword : MonoBehaviour
     public void GrabSword(ObjectAnchor sword)
     {
         for (int i = 0; i < sword.collisionBoxes.Length; i++) { sword.collisionBoxes[i].enabled = true; }
-        //hammer.collisionBoxes[1].isTrigger = false;
-        //Debug.Log(hammer.collisionBoxes.Length);
-        //hammer.collisionBoxes[tip_index].enabled = true; // activate only the endpoint
-        //hammer.collisionBoxes[1].enabled = true; // activate only the endpoint
-        //hammer.collisionBoxes[0].enabled = true;
+
     }
 }
