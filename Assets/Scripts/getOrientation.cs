de2f590222837087474ba0d91cb84d4758c1098c
Antoine251 <antoine.laborde@neuf.fr>
2023-05-13 22:25:20 +0200

    Add objects for container + attempt fix throwing

---
+++ Assets/Scripts/getOrientation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getOrientation : MonoBehaviour
+{
+
+    public Transform trackingSpace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = trackingSpace.rotation;
+        //trackingSpace.Rotate(90.0f, 0.0f, 0.0f);
+        //transform.rotation = trackingSpace.rotation;
+    }
+}
