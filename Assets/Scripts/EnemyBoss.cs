147678392783b712a7a4d9c9e476f905b0f4e254
Basile77 <basile.pasquale@epfl.ch>
2023-05-15 10:07:23 +0200

    killBossFinal

--- Assets/Scripts/EnemyBoss.cs
+++ Assets/Scripts/EnemyBoss.cs
@@ -16,7 +16,7 @@ public class EnemyBoss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
         target = GameObject.FindGameObjectWithTag("Player");
@@ -55,7 +55,8 @@ public class EnemyBoss : MonoBehaviour
 
     void Dying()
     {
-        anim.SetBool("IsDying", false);
+        anim.SetBool("IsWalking", false);
+        anim.SetBool("IsDying", true);
         agent.isStopped = true; 
     }
 
